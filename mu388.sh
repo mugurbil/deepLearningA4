6d648f9fe714cad0755f516909c8a779350542f9
mugurbil <mu388@nyu.edu>
2015-04-29 20:10:42 -0400

    run

--- mu388.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime=12:00:00
-#PBS -l mem=64GB
-
-th main.lua
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-th main.lua
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=12:00:00
+#PBS -l mem=64GB
+
+th query_characters.lua
